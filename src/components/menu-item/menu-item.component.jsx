6e747332e447b32bf77cc52f3e6a094f976dc68b
Bogdan Cristian Burci <bogdanburci81@gmail.com>
2020-06-04 18:54:20 +0300

    updating shop page

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./menu-item.styles.scss";
+import {withRouter} from 'react-router-dom';
+
+const MenuItem = ({ title, imageUrl,size, history , linkUrl, match}) => (
+  <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
+  <div className='background-image'  style={{ backgroundImage: `url(${imageUrl})` }}></div>
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <h1 className="subtitle">SHOP NOW</h1>
+    </div>
+  </div>
+);
+
+export default withRouter(MenuItem);
